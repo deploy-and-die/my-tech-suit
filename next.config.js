0fb677ab4a3ee0f309c0cdb57995e85c09c49e16
Syed Zaid Ali <syedzaidali@thetiza.com>
2026-07-14 00:49:11 +0530

    Configure deployable build output

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Sites packages the framework output from this conventional directory.
+  distDir: 'dist',
+};
 
 if (process.env.DOCKER_BUILD === 'true') {
   nextConfig.output = 'standalone';
